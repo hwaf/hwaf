0e11238d3f6cb2a40a3b37413e63e02c4ccb4a72
Sebastien Binet <binet@cern.ch>
2012-11-19 17:16:49 +0100

    valgrind: first impl

---
+++ find_valgrind.py
@@ -0,0 +1,88 @@
+# -*- python -*-
+
+# stdlib imports ---
+import os
+import os.path as osp
+
+# waf imports ---
+import waflib.Utils
+import waflib.Logs as msg
+from waflib.Configure import conf
+
+#
+_heptooldir = osp.dirname(osp.abspath(__file__))
+
+def options(ctx):
+    ctx.load('hep-waftools-base', tooldir=_heptooldir)
+    ctx.add_option(
+        '--with-valgrind',
+        default=None,
+        help="Look for Valgrind at the given path")
+    return
+
+def configure(ctx):
+    ctx.load('hep-waftools-base', tooldir=_heptooldir)
+    return
+
+@conf
+def find_valgrind(ctx, **kwargs):
+    
+    if not ctx.env.CC or not ctx.env.CXX:
+        msg.fatal('load a C or C++ compiler first')
+        pass
+
+    ctx.load('hep-waftools-base', tooldir=_heptooldir)
+
+    path_list = []
+    if ctx.options.with_valgrind:
+        path_list.append(
+            osp.join(ctx.options.with_valgrind, "bin")
+            )
+        pass
+
+    kwargs['mandatory']=kwargs.get('mandatory', False)
+    kwargs['path_list']=path_list
+
+    
+    ctx.find_program(
+        'valgrind', 
+        var='VALGRIND',
+        **kwargs)
+    
+    ctx.check_with(
+        ctx.check_cfg,
+        "valgrind",
+        package="valgrind",
+        uselib_store="valgrind",
+        args='--cflags --libs',
+        **kwargs)
+
+    # test valgrind
+    version = ctx.check_cxx(
+        msg="Checking valgrind",
+        okmsg="ok",
+        fragment='''\
+        #include "valgrind/config.h"
+        #include "valgrind/valgrind.h"
+        #include <iostream>
+
+        int main(int /*argc*/, char** /*argv*/) {
+          std::cout << PACKAGE_VERSION << std::flush;
+          return 0;
+        }
+        ''',
+        use="valgrind",
+        define_name = "HEPWAF_VALGRIND_VERSION",
+        define_ret = True,
+        execute  = True,
+        mandatory= kwargs['mandatory'],
+        )
+    ctx.start_msg("valgrind version")
+    ctx.end_msg(version)
+
+    ctx.env.VALGRIND_VERSION = version
+
+    ctx.env.HEPWAF_FOUND_VALGRIND = 1
+    return
+
+## EOF ##
