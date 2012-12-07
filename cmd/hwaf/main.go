329e5ca74af1bf9deb3ee5f752888bcd262d86af
Sebastien Binet <binet@cern.ch>
2012-12-07 17:56:55 +0100

    hwaf: first import

---
+++ cmd/hwaf/main.go
@@ -0,0 +1,40 @@
+/*
+hwaf manages hep-waf based applications and libraries.
+
+ Subcommands:
+
+ init [directory]
+
+ self-update
+
+ configure
+
+ build/make
+
+ install
+
+ bdist
+ bdist-rpm
+ bdist-dmg
+ bdist-deb
+
+ run
+
+ shell
+
+ setup
+
+ add-pkg <pkg-uri> [<pkg-version>]
+ checkout|co == alias(add-pkg)
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("::: hwaf...\n")
+
+	return
+}
