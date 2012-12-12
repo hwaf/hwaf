504d2b5878ab68be213e6b1d9e5a1a0ece8523af
Sebastien Binet <binet@cern.ch>
2012-12-12 11:48:30 +0100

    add waf-run

---
+++ cmd_waf_run.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/sbinet/go-commander"
+	"github.com/sbinet/go-flag"
+)
+
+func hwaf_make_cmd_waf_run() *commander.Command {
+	cmd := &commander.Command{
+		Run:       hwaf_run_cmd_waf_run,
+		UsageLine: "run",
+		Short:     "run a command with the correct (project) environment",
+		Long: `
+run a command with the correct (project) environment.
+
+ex:
+ $ hwaf run some-command --some-flag some-data
+`,
+		Flag:        *flag.NewFlagSet("hwaf-waf-run", flag.ExitOnError),
+		CustomFlags: true,
+	}
+	return cmd
+}
+
+func hwaf_run_cmd_waf_run(cmd *commander.Command, args []string) {
+	var err error
+	//n := "hwaf-" + cmd.Name()
+
+	top := hwaf_root()
+	waf := filepath.Join(top, "bin", "waf")
+	if !path_exists(waf) {
+		err = fmt.Errorf(
+			"no such file [%s]\nplease re-run 'hwaf self-init'\n",
+			waf,
+		)
+		handle_err(err)
+	}
+
+	subargs := append([]string{"run"}, args...)
+	sub := exec.Command(waf, subargs...)
+	sub.Stdin = os.Stdin
+	sub.Stdout = os.Stdout
+	sub.Stderr = os.Stderr
+	err = sub.Run()
+	handle_err(err)
+}
+
+// EOF
