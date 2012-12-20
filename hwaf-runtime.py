c9db35a067121cda698ae7917d8e6c4f2b4d3ebd
Sebastien Binet <binet@cern.ch>
2012-12-20 16:05:29 +0100

    dump-env: typo

--- hwaf-runtime.py
+++ hwaf-runtime.py
@@ -491,7 +491,7 @@ class DumpEnvCmdContext(waflib.Build.BuildContext):
         self.hwaf_setup_runtime()
         ret = hwaf_run_cmd_with_runtime_env(
             self, 
-            ctx.env.PYTHON, "-c", """\
+            self.env.PYTHON, "-c", """\
 import json
 import os
 import sys
