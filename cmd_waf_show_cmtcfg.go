6b78e0b67ca3801cde326ba80bd4bc44297be8af
Sebastien Binet <binet@cern.ch>
2013-01-07 18:12:36 +0100

    show-cmtcfg: first stab

---
+++ cmd_waf_show_cmtcfg.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/sbinet/go-commander"
+	"github.com/sbinet/go-flag"
+)
+
+func hwaf_make_cmd_waf_show_cmtcfg() *commander.Command {
+	cmd := &commander.Command{
+		Run:       hwaf_run_cmd_waf_show_cmtcfg,
+		UsageLine: "cmtcfg",
+		Short:     "show local project's CMTCFG value",
+		Long: `
+show cmtcfg displays the project's CMTCFG value.
+
+ex:
+ $ hwaf show cmtcfg
+ x86_64-linux-gcc-opt
+`,
+		Flag:        *flag.NewFlagSet("hwaf-waf-show-cmtcfg", flag.ExitOnError),
+	}
+	return cmd
+}
+
+func hwaf_run_cmd_waf_show_cmtcfg(cmd *commander.Command, args []string) {
+	var err error
+	//n := "hwaf-" + cmd.Name()
+
+	workdir, err := get_workarea_root()
+	handle_err(err)
+
+	// FIXME: get actual value from waf, somehow
+	pinfo_name := filepath.Join(workdir, "__build__", "c4che", "_cache.py")
+	if !path_exists(pinfo_name) {
+		err = fmt.Errorf(
+			"no such file [%s]. did you run \"hwaf configure\" ?",
+			pinfo_name,
+			)
+		handle_err(err)
+	}
+	
+	pinfo, err := NewProjectInfo(pinfo_name)
+	handle_err(err)
+	cmtcfg, err := pinfo.Get("CMTCFG")
+	handle_err(err)
+
+	fmt.Printf("%s\n", cmtcfg)
+}
+
+// EOF
