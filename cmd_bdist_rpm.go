65fc598f0090ca6b342578cb27a47353e89b861d
Sebastien Binet <binet@cern.ch>
2013-01-09 13:12:00 +0100

    rpm: 32b arch is spelled i686. not i386

--- cmd_bdist_rpm.go
+++ cmd_bdist_rpm.go
@@ -253,7 +253,7 @@ rm -rf %{buildroot}
 	case "amd64":
 		rpmarch = "x86_64"
 	case "i386":
-		rpmarch = "i386"
+		rpmarch = "i686"
 	default:
 		err = fmt.Errorf("unhandled GOARCH [%s]", runtime.GOARCH)
 		handle_err(err)
