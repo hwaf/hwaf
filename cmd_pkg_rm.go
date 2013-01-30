94af718235083611251e1bac44f5c41835f6178e
Sebastien Binet <binet@cern.ch>
2013-01-30 16:02:42 +0100

    pkg-rm: remove multiple packages in one go

--- cmd_pkg_rm.go
+++ cmd_pkg_rm.go
@@ -34,23 +34,13 @@ ex:
 func hwaf_run_cmd_pkg_rm(cmd *commander.Command, args []string) {
 	var err error
 	n := "hwaf-pkg-" + cmd.Name()
-	pkgname := ""
 	switch len(args) {
-	case 1:
-		pkgname = args[0]
-	default:
-		err = fmt.Errorf("%s: you need to give a package name to remove", n)
+	case 0:
+		err = fmt.Errorf("%s: you need to give (at least) one package name to remove", n)
 		handle_err(err)
 	}
 
-	pkgname = os.ExpandEnv(pkgname)
-	pkgname = filepath.Clean(pkgname)
-
 	quiet := cmd.Flag.Lookup("q").Value.Get().(bool)
-
-	if !quiet {
-		fmt.Printf("%s: remove package [%s]...\n", n, pkgname)
-	}
 
 	cfg, err := g_ctx.LocalCfg()
 	handle_err(err)
@@ -61,102 +51,133 @@ func hwaf_run_cmd_pkg_rm(cmd *commander.Command, args []string) {
 		handle_err(err)
 	}
 
-	pkg := pkgname
-	if !g_ctx.PkgDb.HasPkg(pkg) {
-		pkg = filepath.Join(srcdir, pkgname)
-	}
-	if !g_ctx.PkgDb.HasPkg(pkg) {
-		err = fmt.Errorf("%s: no such package [%s] in db", n, pkg)
-		handle_err(err)
-	}
+	do_rm := func(pkgname string) error {
+		var err error
 
-	// people may have already removed it via a simple 'rm -rf foo'...
-	//
-	// if !path_exists(pkg) {
-	// 	pkg = filepath.Join(srcdir, pkgname)
-	// 	if !path_exists(pkg) {
-	// 		err = fmt.Errorf("%s: no such package [%s]", n, pkgname)
-	// 		handle_err(err)
-	// 	}
-	// }
-
-	if !g_ctx.PkgDb.HasPkg(pkg) {
-		err = fmt.Errorf("%s: no such package [%s] in db", n, pkg)
-		handle_err(err)
-	}
-
-	vcspkg, err := g_ctx.PkgDb.GetPkg(pkg)
-	handle_err(err)
-
-	switch vcspkg.Type {
-
-	case "git":
-		rmcmd := []string{"rm-submodule"}
+		pkgname = os.ExpandEnv(pkgname)
+		pkgname = filepath.Clean(pkgname)
 		if !quiet {
-			rmcmd = append(rmcmd, "--verbose")
+			fmt.Printf("%s: remove package [%s]...\n", n, pkgname)
 		}
-		rmcmd = append(rmcmd, pkg)
-		git := exec.Command("git", rmcmd...)
+
+		pkg := pkgname
+		if !g_ctx.PkgDb.HasPkg(pkg) {
+			pkg = filepath.Join(srcdir, pkgname)
+		}
+		if !g_ctx.PkgDb.HasPkg(pkg) {
+			return fmt.Errorf("%s: no such package [%s] in db", n, pkg)
+		}
+
+		// people may have already removed it via a simple 'rm -rf foo'...
+		//
+		// if !path_exists(pkg) {
+		// 	pkg = filepath.Join(srcdir, pkgname)
+		// 	if !path_exists(pkg) {
+		// 		err = fmt.Errorf("%s: no such package [%s]", n, pkgname)
+		// 		handle_err(err)
+		// 	}
+		// }
+
+		if !g_ctx.PkgDb.HasPkg(pkg) {
+			return fmt.Errorf("%s: no such package [%s] in db", n, pkg)
+		}
+
+		vcspkg, err := g_ctx.PkgDb.GetPkg(pkg)
+		if err != nil {
+			return err
+		}
+
+		switch vcspkg.Type {
+
+		case "git":
+			rmcmd := []string{"rm-submodule"}
+			if !quiet {
+				rmcmd = append(rmcmd, "--verbose")
+			}
+			rmcmd = append(rmcmd, pkg)
+			git := exec.Command("git", rmcmd...)
+			if !quiet {
+				git.Stdin = os.Stdin
+				git.Stdout = os.Stdout
+				git.Stderr = os.Stderr
+			}
+			err = git.Run()
+			if err != nil {
+				return err
+			}
+
+		case "svn":
+			if path_exists(vcspkg.Path) {
+				err = os.RemoveAll(vcspkg.Path)
+				if err != nil {
+					return err
+				}
+				path := vcspkg.Path
+				// clean-up dir if empty...
+				for {
+					path = filepath.Dir(path)
+					if path == srcdir {
+						break
+					}
+					content, err := filepath.Glob(filepath.Join(path, "*"))
+					if err != nil {
+						return err
+					}
+					if len(content) > 0 {
+						break
+					}
+					err = os.RemoveAll(path)
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+		case "local":
+			if path_exists(vcspkg.Path) {
+				err = os.RemoveAll(vcspkg.Path)
+				if err != nil {
+					return err
+				}
+				path := vcspkg.Path
+				// clean-up dir if empty...
+				for {
+					path = filepath.Dir(path)
+					if path == srcdir {
+						break
+					}
+					content, err := filepath.Glob(filepath.Join(path, "*"))
+					if err != nil {
+						return err
+					}
+					if len(content) > 0 {
+						break
+					}
+					err = os.RemoveAll(path)
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+		default:
+			return fmt.Errorf("%s: VCS of type [%s] is not handled", n, vcspkg.Type)
+		}
+
+		err = g_ctx.PkgDb.Remove(pkg)
+		if err != nil {
+			return err
+		}
+
 		if !quiet {
-			git.Stdin = os.Stdin
-			git.Stdout = os.Stdout
-			git.Stderr = os.Stderr
+			fmt.Printf("%s: remove package [%s]... [ok]\n", n, pkgname)
 		}
-		err = git.Run()
-		handle_err(err)
-
-	case "svn":
-		if path_exists(vcspkg.Path) {
-			err = os.RemoveAll(vcspkg.Path)
-			handle_err(err)
-			path := vcspkg.Path
-			// clean-up dir if empty...
-			for {
-				path = filepath.Dir(path)
-				if path == srcdir {
-					break
-				}
-				content, err := filepath.Glob(filepath.Join(path, "*"))
-				handle_err(err)
-				if len(content) > 0 {
-					break
-				}
-				err = os.RemoveAll(path)
-				handle_err(err)
-			}
-		}
-
-	case "local":
-		if path_exists(vcspkg.Path) {
-			err = os.RemoveAll(vcspkg.Path)
-			handle_err(err)
-			path := vcspkg.Path
-			// clean-up dir if empty...
-			for {
-				path = filepath.Dir(path)
-				if path == srcdir {
-					break
-				}
-				content, err := filepath.Glob(filepath.Join(path, "*"))
-				handle_err(err)
-				if len(content) > 0 {
-					break
-				}
-				err = os.RemoveAll(path)
-				handle_err(err)
-			}
-		}
-
-	default:
-		err = fmt.Errorf("%s: VCS of type [%s] is not handled", n, vcspkg.Type)
-		handle_err(err)
+		return nil
 	}
 
-	err = g_ctx.PkgDb.Remove(pkg)
-	handle_err(err)
-
-	if !quiet {
-		fmt.Printf("%s: remove package [%s]... [ok]\n", n, pkgname)
+	for _, pkgname := range args {
+		err := do_rm(pkgname)
+		handle_err(err)
 	}
 }
 
