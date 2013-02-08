8714dc247aa86a3833b886942ff2710a1b9769ff
Sebastien Binet <binet@cern.ch>
2013-02-08 10:47:56 +0100

    gdb: first impl

---
+++ find_gdb.py
@@ -0,0 +1,40 @@
+# -*- python -*-
+
+# stdlib imports ---
+import os
+import os.path as osp
+
+# waf imports ---
+import waflib.Utils
+import waflib.Logs as msg
+from waflib.Configure import conf
+
+#
+_heptooldir = osp.dirname(osp.abspath(__file__))
+
+def options(ctx):
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+    return
+
+def configure(ctx):
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+    return
+
+@conf
+def find_gdb(ctx, **kwargs):
+    
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+
+    # find gdb executable
+    gdb = "gdb"
+    if getattr(ctx.options, 'with_gdb', None):
+        gdb = osp.abspath(
+            osp.join(ctx.options.with_gdb, "bin", "gdb")
+            )
+        pass
+    ctx.find_program(gdb, var='GDB',**kwargs)
+
+    ctx.env.HWAF_FOUND_GDB = 1
+    return
+
+## EOF ##
