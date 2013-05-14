86032b764ef6e5145ee3420b2b67bcff990e827b
Sebastien Binet <binet@cern.ch>
2013-05-14 14:39:42 +0200

    tar: migrate to go-1.1 and archive/tar.FileInfoHeader

--- utils_tar.go
+++ utils_tar.go
@@ -5,92 +5,14 @@ package main
 import (
 	"archive/tar"
 	"compress/gzip"
-	"errors"
 	"fmt"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
-// Mode constants from the tar spec. 
-const (
-	c_ISDIR  = 040000
-	c_ISFIFO = 010000
-	c_ISREG  = 0100000
-	c_ISLNK  = 0120000
-	c_ISBLK  = 060000
-	c_ISCHR  = 020000
-	c_ISSOCK = 0140000
-)
-
-// sysStat, if non-nil, populates h from system-dependent fields of fi. 
-var sysStat func(fi os.FileInfo, h *tar.Header) error
-
-// FileInfoHeader creates a partially-populated Header from fi.
-// If fi describes a symlink, FileInfoHeader records link as the link target.
-func _tar_FileInfoHeader(fi os.FileInfo, link string) (*tar.Header, error) {
-	if fi == nil {
-		return nil, errors.New("tar: FileInfo is nil")
-	}
-	h := &tar.Header{
-		Name:    fi.Name(),
-		ModTime: fi.ModTime(),
-		Mode:    int64(fi.Mode().Perm()), // or'd with c_IS* constants later
-	}
-	switch {
-	case fi.Mode()&os.ModeType == 0:
-		h.Mode |= c_ISREG
-		h.Typeflag = tar.TypeReg
-		h.Size = fi.Size()
-	case fi.IsDir():
-		h.Typeflag = tar.TypeDir
-		h.Mode |= c_ISDIR
-	case fi.Mode()&os.ModeSymlink != 0:
-		h.Typeflag = tar.TypeSymlink
-		h.Mode |= c_ISLNK
-		h.Linkname = link
-	case fi.Mode()&os.ModeDevice != 0:
-		if fi.Mode()&os.ModeCharDevice != 0 {
-			h.Mode |= c_ISCHR
-			h.Typeflag = tar.TypeChar
-		} else {
-			h.Mode |= c_ISBLK
-			h.Typeflag = tar.TypeBlock
-		}
-	case fi.Mode()&os.ModeSocket != 0:
-		h.Mode |= c_ISSOCK
-	default:
-		return nil, fmt.Errorf("archive/tar: unknown file mode %v", fi.Mode())
-	}
-	if sysStat != nil {
-		return h, sysStat(fi, h)
-	}
-	return h, nil
-}
-
 func _tar_gz(targ, workdir string) error {
-	// FIXME: use archive/tar instead (once go-1.1 is out)
-	{
-		matches, err := filepath.Glob(filepath.Join(workdir, "*"))
-		if err != nil {
-			return err
-		}
-		for i, m := range matches {
-			matches[i] = m[len(workdir)+1:]
-		}
-		args := []string{}
-		args = append(args, "-zcf", targ)
-		args = append(args, matches...)
-		//fmt.Printf(">> %v\n", args)
-		cmd := exec.Command("tar", args...)
-		cmd.Dir = workdir
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		return cmd.Run()
-	}
 
 	f, err := os.Create(targ)
 	if err != nil {
@@ -117,7 +39,10 @@ func _tar_gz(targ, workdir string) error {
 		// 	name = filepath.Join(prefix, name)
 		// }
 		target, _ := os.Readlink(path)
-		hdr, err := _tar_FileInfoHeader(fi, target)
+		// if err != nil {
+		// 	return err
+		// }
+		hdr, err := tar.FileInfoHeader(fi, target)
 		if err != nil {
 			return err
 		}
