ee74784a9d475929c8e47406faac8387b5604c90
Sebastien Binet <binet@cern.ch>
2013-07-04 17:08:38 +0200

    test: test pkg-co and pkg-rm scenarii

--- all_test.go
+++ all_test.go
@@ -135,6 +135,70 @@ func TestBasicState(t *testing.T) {
 	}
 }
 
+func TestPkgCoRm(t *testing.T) {
+	workdir, err := ioutil.TempDir("", "hwaf-test-")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer os.RemoveAll(workdir)
+
+	err = os.Chdir(workdir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	hwaf, err := newlogger("hwaf.log")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer hwaf.Close()
+
+	for _, cmd := range [][]string{
+		{"hwaf", "init", "-q=0", "."},
+		{"hwaf", "setup", "-q=0"},
+		{"hwaf", "configure"},
+		{"hwaf", "build"},
+		{"hwaf", "install"},
+		{"hwaf", "pkg", "co", "git://github.com/hwaf/hwaf-tests-pkg-settings", "pkg-settings"},
+	} {
+		err := hwaf.Run(cmd[0], cmd[1:]...)
+		if err != nil {
+			hwaf.Display()
+			t.Fatalf("cmd %v failed: %v", cmd, err)
+		}
+	}
+
+	// test adding the same package
+	err = hwaf.Run("hwaf", "pkg", "co", "git://github.com/hwaf/hwaf-tests-pkg-settings", "pkg-settings")
+	if err == nil {
+		t.Fatalf("hwaf pkg co should have FAILED!")
+	}
+
+	// test removing the package "by hand"
+	err = os.RemoveAll(filepath.Join(workdir, "src", "pkg-settings"))
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	err = hwaf.Run("hwaf", "pkg", "rm", "pkg-settings")
+	if err == nil {
+		t.Fatalf("hwaf pkg rm should have FAILED!")
+	}
+
+	// recover
+	err = hwaf.Run("hwaf", "pkg", "rm", "-f", "pkg-settings")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// add it back
+	err = hwaf.Run("hwaf", "pkg", "co", "git://github.com/hwaf/hwaf-tests-pkg-settings", "pkg-settings")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+}
+
 func TestHwafTuto(t *testing.T) {
 	workdir, err := ioutil.TempDir("", "hwaf-test-")
 	if err != nil {
