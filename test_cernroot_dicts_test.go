6ce7c501b0449ef97899f2d3c9fdb9a64b5c2b05
Sebastien Binet <binet@cern.ch>
2013-07-22 15:14:47 +0200

    test: add reflex test

--- test_cernroot_dicts_test.go
+++ test_cernroot_dicts_test.go
@@ -53,4 +53,54 @@ func TestCernRootCint(t *testing.T) {
 	}
 }
 
+func TestCernRootReflex(t *testing.T) {
+	{
+		if err := exec.Command("which", "root-config").Run(); err != nil {
+			t.Skip("skipping test. (needs CERN-ROOT (missing root-config))")
+
+		}
+		if err := exec.Command("which", "genreflex").Run(); err != nil {
+			t.Skip("skipping test. (needs CERN-ROOT) (missing genreflex)")
+
+		}
+		if err := exec.Command("which", "gccxml").Run(); err != nil {
+			t.Skip("skipping test. (needs CERN-ROOT) (missing gccxml)")
+
+		}
+	}
+	workdir, err := ioutil.TempDir("", "hwaf-test-")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer os.RemoveAll(workdir)
+
+	err = os.Chdir(workdir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	hwaf, err := newlogger("hwaf.log")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer hwaf.Close()
+
+	for _, cmd := range [][]string{
+		{"hwaf", "init", "-q=0", "."},
+		{"hwaf", "setup", "-q=0"},
+		{"hwaf", "pkg", "co", "-q=0", "git://github.com/hwaf/hwaf-tests-pkg-settings", "pkg-settings"},
+		{"hwaf", "pkg", "co", "-q=0", "git://github.com/hwaf/hwaf-tests-rootreflex-pkg1", "pkg1"},
+		{"hwaf", "pkg", "co", "-q=0", "git://github.com/hwaf/hwaf-tests-rootreflex-pkg2", "pkg2"},
+		{"hwaf", "configure"},
+		{"hwaf"},
+		{"hwaf", "run", "root", "-l", "-b", "src/pkg2/share/run.C"},
+	} {
+		err := hwaf.Run(cmd[0], cmd[1:]...)
+		if err != nil {
+			hwaf.Display()
+			t.Fatalf("cmd %v failed: %v", cmd, err)
+		}
+	}
+}
+
 // EOF
