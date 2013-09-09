cf3ae4bd2fa9aa1a7ce1941fd1cb9b920aac2329
Sebastien Binet <binet@cern.ch>
2013-09-09 14:56:06 +0200

    hwaf-rules: add a task to export lib defs to a project.infos

--- py-hwaftools/hwaf-rules.py
+++ py-hwaftools/hwaf-rules.py
@@ -121,6 +121,27 @@ def hwaf_install_headers(self):
                                [incpath,inc_node.parent.abspath()])
     return
 
+### ---------------------------------------------------------------------------
+@after_method('apply_link')
+@feature('hwaf_export_lib')
+def hwaf_export_lib(self):
+    """
+    A task to properly export a library definition.
+    """
+    # extract package name
+    pkgdir = self.path.abspath()
+    pkgname = self.bld.hwaf_pkg_name(pkgdir)
+    b_pkgname = osp.basename(pkgname)
+    if not hasattr(self, 'link_task'):
+        msg.debug('hwaf: task [%s] has no link_task' % self.name)
+        return
+
+    uses = [self.name] + waflib.Utils.to_list(getattr(self, 'use', []))
+    self.bld.env.append_unique('LIB_%s' % self.name, uses)
+    #self.env.append_unique('LIB_%s' % self.name, uses)
+    #msg.debug('hwaf: export-lib: %s => %r' % (self.name, uses))
+    return
+
 ### -----------------------------------------------------------------------------
 import waflib.Build
 class InstallContext(waflib.Build.InstallContext):
