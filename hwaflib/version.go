776a085bac25a936fc68a9d3cd58cb25230d1bb3
Sebastien Binet <binet@cern.ch>
2013-09-11 18:51:23 +0200

    version: 20130911

--- hwaflib/version.go
+++ hwaflib/version.go
@@ -1,12 +1,12 @@
 package hwaflib
 
 func (ctx *Context) Version() string {
-	version := "20130909"
+	version := "20130911"
 	return version
 }
 
 func (ctx *Context) Revision() string {
-	revision := "12f77c8"
+	revision := "4f32e27"
 	return revision
 }
 
