883ca399804ae7e3775d31fdc664228569276c84
Sebastien Binet <binet@cern.ch>
2013-11-16 13:03:27 +0100

    test: amsg lcg + amsg standalone

--- test_hwaf_amsg_test.go
+++ test_hwaf_amsg_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestAmsgBuild(t *testing.T) {
+func TestAmsgBuildLcg(t *testing.T) {
 
 	workdir, err := ioutil.TempDir("", "hwaf-test-")
 	if err != nil {
@@ -38,8 +38,46 @@ func TestAmsgBuild(t *testing.T) {
 		t.Fatalf(err.Error())
 	}
 
-	err = hwaf.Run("/bin/sh", "build.sh")
+	err = hwaf.Run("/bin/sh", "build.sh", "lcg")
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 }
+
+func TestAmsgBuildStandalone(t *testing.T) {
+
+	workdir, err := ioutil.TempDir("", "hwaf-test-")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer os.RemoveAll(workdir)
+	//fmt.Printf(">>> test: %s\n", workdir)
+
+	err = os.Chdir(workdir)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	hwaf, err := newlogger("hwaf.log")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer hwaf.Close()
+
+	err = hwaf.Run("git", "clone", "git://github.com/mana-fwk/amsg-build")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	err = os.Chdir("amsg-build")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	err = hwaf.Run("/bin/sh", "build.sh", "standalone")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+// EOF
