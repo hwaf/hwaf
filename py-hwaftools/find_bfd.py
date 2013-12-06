c08c366229b6048d6834ecdfe95d57f7273f199c
Sebastien Binet <binet@cern.ch>
2013-12-06 10:32:47 +0100

    all: split find_posixlibs into find_iberty,find_bfd

---
+++ py-hwaftools/find_bfd.py
@@ -0,0 +1,108 @@
+# -*- python -*-
+
+# stdlib imports ---
+import os
+import os.path as osp
+
+# waf imports ---
+import waflib.Utils
+import waflib.Logs as msg
+from waflib.Configure import conf
+
+#
+_heptooldir = osp.dirname(osp.abspath(__file__))
+
+def options(ctx):
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+    ctx.load('find_gdb', tooldir=_heptooldir)
+    ctx.load('find_iberty', tooldir=_heptooldir)
+    ctx.load('find_z', tooldir=_heptooldir)
+    ctx.load('find_dl', tooldir=_heptooldir)
+    return
+
+def configure(ctx):
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+    ctx.load('find_gdb', tooldir=_heptooldir)
+    ctx.load('find_iberty', tooldir=_heptooldir)
+    ctx.load('find_z', tooldir=_heptooldir)
+    ctx.load('find_dl', tooldir=_heptooldir)
+    return
+
+@conf
+def find_bfd(ctx, **kwargs):
+    
+    ctx.load('hwaf-base', tooldir=_heptooldir)
+    ctx.load('find_gdb', tooldir=_heptooldir)
+    ctx.load('find_iberty', tooldir=_heptooldir)
+    ctx.load('find_z', tooldir=_heptooldir)
+    ctx.load('find_dl', tooldir=_heptooldir)
+
+    if not ctx.env.HWAF_FOUND_C_COMPILER:
+        ctx.fatal('load a C compiler first')
+        pass
+
+    if not ctx.env.HWAF_FOUND_GDB:
+        # gdb might have installed interesting libs (iberty, bfd)
+        ctx.find_gdb(mandatory=False)
+        pass
+    
+    if not ctx.env.HWAF_FOUND_Z:
+        ctx.find_z(mandatory=True)
+        pass
+
+    if not ctx.env.HWAF_FOUND_DL:
+        ctx.find_dl(mandatory=True)
+        pass
+
+    if not ctx.env.HWAF_FOUND_IBERTY:
+        ctx.find_iberty(mandatory=True)
+        pass
+
+    extra_paths = waflib.Utils.to_list(kwargs.get('extra_paths',[]))
+    if ctx.env.GDB_HOME:
+        gdb = ctx.hwaf_subst_vars('${GDB_HOME}')
+        extra_paths.append(gdb)
+        pass
+
+    # find bfd
+    ctx.check_with(
+        ctx.check,
+        "bfd",
+        features='c cstlib',
+        defines=['PACKAGE="package-name"','PACKAGE_VERSION="package-version"',],
+        header_name="bfd.h",
+        stlib='bfd',
+        uselib_store='bfd',
+        use='dl iberty',
+        **kwargs
+        )
+    ctx.env.DEFINES_bfd = []
+
+    # test bfd
+    ctx.check_cc(
+        msg="Checking bfd_init",
+        okmsg="ok",
+        features='c cstlib',
+        fragment='''\
+        #ifndef PACKAGE
+        # define PACKAGE "package-name"
+        #endif
+        #ifndef PACKAGE_VERSION
+        # define PACKAGE_VERSION 1
+        #endif
+        #include "bfd.h"
+
+        int test_bfd_init() {
+          bfd_init();
+          bfd_error_type err = bfd_get_error();
+          return (int)err;
+        }
+        ''',
+        use=['z', 'bfd', 'iberty', 'dl'],
+        mandatory= kwargs['mandatory'],
+        )
+
+    ctx.env.HWAF_FOUND_BFD = 1
+    return
+
+## EOF ##
--- py-hwaftools/find_posixlibs.py
+++ py-hwaftools/find_posixlibs.py
@@ -17,9 +17,10 @@ def options(ctx):
     ctx.load('find_pthread', tooldir=_heptooldir)
     ctx.load('find_m', tooldir=_heptooldir)
     ctx.load('find_dl', tooldir=_heptooldir)
-    ctx.load('find_gdb', tooldir=_heptooldir)
     ctx.load('find_z', tooldir=_heptooldir)
     ctx.load('find_rt', tooldir=_heptooldir)
+    ctx.load('find_iberty', tooldir=_heptooldir)
+    ctx.load('find_bfd', tooldir=_heptooldir)
     return
 
 def configure(ctx):
@@ -27,9 +28,10 @@ def configure(ctx):
     ctx.load('find_pthread', tooldir=_heptooldir)
     ctx.load('find_m', tooldir=_heptooldir)
     ctx.load('find_dl', tooldir=_heptooldir)
-    ctx.load('find_gdb', tooldir=_heptooldir)
     ctx.load('find_z', tooldir=_heptooldir)
     ctx.load('find_rt', tooldir=_heptooldir)
+    ctx.load('find_iberty', tooldir=_heptooldir)
+    ctx.load('find_bfd', tooldir=_heptooldir)
     return
 
 @conf
@@ -41,11 +43,6 @@ def find_posixlibs(ctx, **kwargs):
         ctx.fatal('load a C compiler first')
         pass
 
-    if not ctx.env.HWAF_FOUND_GDB:
-        # gdb might have installed interesting libs (iberty, bfd)
-        ctx.find_gdb(mandatory=False)
-        pass
-    
     # find libm
     libm_kw = dict(kwargs)
     libm_kw['mandatory'] = libm_kw.get('m_mandatory', False)
@@ -71,79 +68,15 @@ def find_posixlibs(ctx, **kwargs):
     librt_kw['mandatory'] = librt_kw.get('rt_mandatory', False)
     ctx.find_rt(**librt_kw)
 
-    extra_paths = waflib.Utils.to_list(kwargs.get('extra_paths',[]))
-    if ctx.env.GDB_HOME:
-        gdb = ctx.hwaf_subst_vars('${GDB_HOME}')
-        extra_paths.append(gdb)
-        pass
-
     # find libiberty
-    iberty_mandatory = kwargs.get('iberty_mandatory', False)
-    if ctx.is_darwin():
-        iberty_mandatory = kwargs.get('iberty_mandatory', False)
-        pass
     iberty_kwargs = dict(kwargs)
-    iberty_kwargs['mandatory'] = iberty_mandatory
-    iberty_kwargs['extra_paths'] = extra_paths
-    
-    ctx.check_with(
-        ctx.check,
-        "iberty",
-        features='c cstlib',
-        header_name="libiberty.h",
-        stlib='iberty',
-        uselib_store='iberty',
-        **iberty_kwargs
-        )
-    if ctx.env.LIBPATH_iberty and 0:
-        ctx.env.STLIBPATH_iberty = ctx.env.LIBPATH_iberty
-        ctx.env.STLIB_iberty = ctx.env.LIB_iberty
-        ctx.env.LIB_iberty = []
-        ctx.env.LIBPATH_iberty = []
-        pass
+    iberty_kwargs['mandatory'] = kwargs.get('iberty_mandatory', False)
+    ctx.find_iberty(**iberty_kwargs)
 
     # find bfd
-    bfd_mandatory = kwargs.get('bfd_mandatory', False)
-    bfd_kwargs = dict(kwargs)
-    bfd_kwargs['mandatory'] = bfd_mandatory
-    bfd_kwargs['extra_paths'] = extra_paths
-    
-    ctx.check_with(
-        ctx.check,
-        "bfd",
-        features='c cstlib',
-        defines=['PACKAGE="package-name"','PACKAGE_VERSION="package-version"',],
-        header_name="bfd.h",
-        stlib='bfd',
-        uselib_store='bfd',
-        use='dl iberty',
-        **bfd_kwargs
-        )
-    ctx.env.DEFINES_bfd = []
-    
-    # test bfd
-    ctx.check_cc(
-        msg="Checking bfd_init",
-        okmsg="ok",
-        features='c cstlib',
-        fragment='''\
-        #ifndef PACKAGE
-        # define PACKAGE "package-name"
-        #endif
-        #ifndef PACKAGE_VERSION
-        # define PACKAGE_VERSION 1
-        #endif
-        #include "bfd.h"
-
-        int test_bfd_init() {
-          bfd_init();
-          bfd_error_type err = bfd_get_error();
-          return (int)err;
-        }
-        ''',
-        use=['z', 'bfd', 'iberty', 'dl'],
-        mandatory= bfd_mandatory,
-        )
+    bfd_kw = dict(kwargs)
+    bfd_kw['mandatory'] = kwargs.get('bfd_mandatory', False)
+    ctx.find_bfd(**bfd_kw)
 
     ctx.env.HWAF_FOUND_POSIXLIBS = 1
     return
